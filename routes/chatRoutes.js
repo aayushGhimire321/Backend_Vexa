7a39f23e90c56870e6c78730e88cf51cbe25f76d
Aayush Ghimire <aghimire491@gmail.com>
2025-02-16 18:49:26 +0545

    chat system

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -1,28 +1,9 @@
 import express from "express";
-import Chat from "../models/Chat.js";  // Import Chat Model
+import { getMessages, sendMessage } from "../controllers/chatController.js";
 
 const router = express.Router();
 
-// Get all messages
-router.get("/messages", async (req, res) => {
-    try {
-        const messages = await Chat.find();
-        res.json(messages);
-    } catch (err) {
-        res.status(500).json({ message: "Error retrieving messages" });
-    }
-});
-
-// Send a new message
-router.post("/message", async (req, res) => {
-    const { sender, message } = req.body;
-    try {
-        const newMessage = new Chat({ sender, message });
-        await newMessage.save();
-        res.status(201).json(newMessage);
-    } catch (err) {
-        res.status(500).json({ message: "Error saving message" });
-    }
-});
+router.get("/", getMessages);
+router.post("/", sendMessage);
 
 export default router;
